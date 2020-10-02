c0ce803cf1b9ea74df86dfd074b1759decd52178
j-keck <jhyphenkeck@gmail.com>
2020-10-02 13:15:33 +0200

    print env

--- build.sh
+++ build.sh
@@ -1,1 +1,10 @@
 echo "executed on the codepipeline"
+
+
+echo "------------------------------------------------"
+echo "env:"
+env
+
+echo "------------------------------------------------"
+echo "git env:"
+env | grep -i git
